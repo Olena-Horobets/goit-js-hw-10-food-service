d3ddb3a776e75fddc28b9aac0f0633a955187563
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-08-27 12:40:20 +0300

    menu markup generated

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import './sass/main.scss';
 import { generateCards } from './js/generate-cards';
 
 generateCards();
-
-console.log(85757);
--- src/js/generate-cards.js
+++ src/js/generate-cards.js
@@ -1,5 +1,9 @@
 import { refs } from './refs';
+import menu from '../db/menu.json';
+import menuItemTpl from '../templates/menu-item.hbs';
 
 export const generateCards = function () {
-  console.log(refs);
+  const menuMarkup = menuItemTpl(menu);
+
+  refs.cardsList.insertAdjacentHTML('beforeend', menuMarkup);
 };
